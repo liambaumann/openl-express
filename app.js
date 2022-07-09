c09340bfd198687255d7095be9fc46110864dd05
Liam <lbaumann@outlook.at>
2022-07-09 21:38:00 +0200

    trying to connect to sql

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-require('dotenv').config()  //adding dotenv functionality
+require('dotenv').config() // adding dotenv functionality
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASSWORD});
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World, from express. My secret: ' + process.env.SECRET_MESSAGE);
+    connection.connect();
+
+    connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+        if (err) 
+            throw err;
+        
+        console.log('The solution is: ', rows[0].solution);
+    });
+
+    connection.end();
+    res.send('Hello World, from express. My solution: ' + process.env.SECRET_MESSAGE);
 });
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
