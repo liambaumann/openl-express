ce4369561c0ae34d110b25bbd836c6565a017698
Liam <lbaumann@outlook.at>
2022-07-09 22:07:49 +0200

    connection do database working

--- app.js
+++ app.js
@@ -3,21 +3,36 @@ const app = express();
 const port = 8080;
 require('dotenv').config() // adding dotenv functionality
 
-var mysql = require('mysql');
-var connection = mysql.createConnection({host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASSWORD});
+const mariadb = require('mariadb');
 
 
-app.get('/', (req, res) => {
-    connection.connect();
+const pool = mariadb.createPool({host: process.env.DB_HOST, user: process.env.DB_USER, database: process.env.DB_NAME, password: process.env.DB_PASSWORD, debug: false});
 
-    connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-        if (err) 
-            throw err;
+pool.getConnection()
+    .then(conn => {
+    
+      conn.query("SELECT 1 as val")
+        .then((rows) => {
+          console.log(rows); //[ {val: 1}, meta: ... ]
+          //Table must have been created before 
+          // " CREATE TABLE myTable (id int, val varchar(255)) "
+          return conn.query("INSERT INTO myTable value ('hallo', 1)", [1, "mariadb"]);
+        })
+        .then((res) => {
+          console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+          conn.end();
+        })
+        .catch(err => {
+          //handle error
+          console.log(err); 
+          conn.end();
+        })
         
-        console.log('The solution is: ', rows[0].solution);
+    }).catch(err => {
+      //not connected
     });
 
-    connection.end();
+app.get('/', (req, res) => {
     res.send('Hello World, from express. My solution: ' + process.env.SECRET_MESSAGE);
 });
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
