a4b04be1d6d74cb4612232999cd1138b30d9ac19
Liam <lbaumann@outlook.at>
2022-07-09 20:55:25 +0200

    env and gitignore setup

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+require('dotenv').config()  //adding dotenv functionality
+
 app.get('/', (req, res) => {
-    res.send('Hello World, from express');
+    res.send('Hello World, from express. My secret: ' + process.env.SECRET_MESSAGE);
 });
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
